3862b9130700a6c8240d8cbff801e8df684656aa
TimurMP <titimurmamyshev@gmail.commur>
2022-08-06 02:48:29 +0300

    In Progress

--- src/telran/box/controller/PomegranateAppl.java
+++ src/telran/box/controller/PomegranateAppl.java
@@ -7,6 +7,7 @@ import telran.box.model.Seed;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class PomegranateAppl {
     public static void main(String[] args) {
@@ -20,11 +21,37 @@ public class PomegranateAppl {
 
         }
 
+        System.out.println("\n===========");
+        maxSeedsPerBox(boxes);
+
 
 
 
     }
 
+    private static int maxSeedsPerBox(List<Box> boxes){
+        for (Box box : boxes) {
+            int count = 0;
+            for (Pomegranate pomegranate : box) {
+                count += pomegranate.getSeeds().size();
+            }
+            System.out.println(count);
+        }
+
+        Iterable<Integer> max = boxes.stream()
+                .map(Box::getPomegranates)
+                .flatMap(pomegranates -> pomegranates.stream())
+                .map(x -> x.getSeeds().size())
+                .collect(Collectors.toList());
+
+        System.out.println();
+        for (Integer integer : max) {
+            System.out.println(integer);
+
+        }
+
+        return 0;
+    }
     private static int totalSeeds(List<Box> boxes){
         return  boxes.stream()
                 .map(Box::getPomegranates)
@@ -36,7 +63,7 @@ public class PomegranateAppl {
     }
 
     private static  List<Box> generateBoxes(){
-        int numberOfBoxes = randomGenerator(5, 5);
+        int numberOfBoxes = randomGenerator(1, 5);
         List<Box> list = new ArrayList<>();
         for (int i = 0; i < numberOfBoxes; i++) {
             list.add(new Box("Box" + i, generatePomegranates()));
@@ -46,7 +73,7 @@ public class PomegranateAppl {
 
 
     private static  List<Pomegranate> generatePomegranates(){
-        int numberOfPomegranates = randomGenerator(5, 5);
+        int numberOfPomegranates = randomGenerator(1, 5);
         List<Pomegranate> list = new ArrayList<>();
         for (int i = 0; i < numberOfPomegranates; i++) {
             list.add(new Pomegranate(generateSeeds()));
@@ -55,7 +82,7 @@ public class PomegranateAppl {
     }
 
     private static  List<Seed> generateSeeds(){
-        int numberOfSeeds = randomGenerator(5, 5);
+        int numberOfSeeds = randomGenerator(1, 3);
         List<Seed> list = new ArrayList<>();
         for (int i = 0; i < numberOfSeeds; i++) {
              list.add(new Seed());
